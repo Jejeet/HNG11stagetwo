08a2dc54819d5b201deb0c620a3799cc84a4f564
Adewumi <owonikokoadewunmi@gmail.com>
2024-07-10 16:56:26 +0100

    Updated mobile nav

--- src/components/Hero/Clearpage.tsx
+++ src/components/Hero/Clearpage.tsx
@@ -5,9 +5,9 @@ import { NewProduct } from '../productcards/NewProduct'
 
 const Clearpage = () => {
   return (
-    <div className='text-center bg-[#C4D7ED] py-10'>
+    <div className='text-center bg-[#C4D7ED]  py-10'>
         <h2 className=' pb-4 items-center text-3xl'>CLEARANCE DEALS
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-10 py-7'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-rows-3 lg:grid-cols-4 gap-5 mx-10 py-7 '>
                 {HomeDeal.slice(0, 4).map((HomeDeal)=>{
                    
                         return <Link to={"/product"}><NewProduct {...HomeDeal}/></Link>
@@ -21,3 +21,5 @@ const Clearpage = () => {
 }
 
 export default Clearpage
+
+
