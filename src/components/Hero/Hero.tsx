82c12ccd47faae5f43c49c672cb05a02cfc259d5
Adewumi <owonikokoadewunmi@gmail.com>
2024-07-10 01:12:28 +0100

    update cart

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,13 +3,13 @@ import hero from "@/assets/Hero.png";
 import { Homebutton } from "../Buttons/Homebtn";
 const Hero = () => {
   return (
-    <div >
+    <div className="relative ">
       <div >
         {" "}
-        <img src={hero} alt="" className="relative h-[690px]" />
+        <img src={hero} alt="" className="h-[690px] " />
       </div>
 
-      <div className="absolute top-[195px] bg-[#D9D9D9C9] py-5 px-10  ml-10 items-center ">
+      <div className="absolute top-[85px] bg-[#D9D9D9C9] py-5 px-10  ml-10 items-center ">
         <div className=" " >
           <h2>HomeNest Gallery (HNG)</h2>
           <h2>Redefining Furniture Shopping in <br/> Nigeria</h2> 
